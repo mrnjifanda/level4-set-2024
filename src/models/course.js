3dfd44be5fcc2974212151d2fd264df837d79a2f
wilfried <josephaboh29@gmail.com>
2024-10-18 19:29:16 +0200

    exercise done

---
+++ src/models/course.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+
+const CourseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  duration: {
+    type: Number, 
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Course', CourseSchema);
